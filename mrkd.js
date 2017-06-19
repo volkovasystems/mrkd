c9f626c9a63da565b57db8450aeb3a485f3fc40f
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-19 15:50:14 +0800

    Updated dependency.

--- mrkd.js
+++ mrkd.js
@@ -34,7 +34,7 @@
 			"author": "Richeve S. Bebedor",
 			"contributors": [
 				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>",
-				"Vinse Vinalon"
+				"Vinse Vinalon <vinsevinalon@gmail.com>"
 			],
 			"eMail": "richeve.bebedor@gmail.com",
 			"repository": "https://github.com/volkovasystems/mrkd.git",
